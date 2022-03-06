b1a56589f995e1f2f53045adc921d2f48292814c
EZZEDINE Mohammed <mohammed.ezzedine@murex.com>
2022-03-06 23:25:59 +0200

    Add domain layer to the authentication service

---
+++ MySpace.Authentication/MySpace.Authentication.Domain/Models/User.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MySpace.Authentication.Domain.Models;
+
+public class User : IdentityUser
+{
+
+}
---
+++ MySpace.Authentication/MySpace.Authentication.Domain/Persistence/UserRepository.cs
@@ -0,0 +1,11 @@
+﻿using MySpace.Authentication.Domain.Models;
+
+namespace MySpace.Authentication.Domain.Persistence;
+
+public interface UserRepository
+{
+    User AddUser(User user);
+    User GetUser(string id);
+    User EditUser(string id, User user);
+    void DeleteUser(string id);
+}
