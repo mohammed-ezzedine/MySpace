cac5a3f106309b6e3f164891c8e9f920e2952f9e
EZZEDINE Mohammed <mezzedine@murex.com>
2022-04-19 18:32:32 +0300

    Fix ui issues on small screens

--- MySpace/src/app/components/add-code-snippet/add-code-snippet.component.ts
+++ MySpace/src/app/components/add-code-snippet/add-code-snippet.component.ts
@@ -2,6 +2,44 @@ import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {FormBuilder, FormControl} from "@angular/forms";
 import {ArticleAdditionEvent} from "../../events/article-addition.event";
 
+import 'prismjs/components/prism-aspnet';
+import 'prismjs/components/prism-bash';
+import 'prismjs/components/prism-c';
+import 'prismjs/components/prism-clike';
+import 'prismjs/components/prism-cpp';
+import 'prismjs/components/prism-csharp';
+import 'prismjs/components/prism-css';
+import 'prismjs/components/prism-css-extras';
+import 'prismjs/components/prism-dart';
+import 'prismjs/components/prism-docker';
+import 'prismjs/components/prism-fsharp';
+import 'prismjs/components/prism-git';
+import 'prismjs/components/prism-go';
+import 'prismjs/components/prism-go-module';
+import 'prismjs/components/prism-graphql';
+import 'prismjs/components/prism-http';
+import 'prismjs/components/prism-ignore';
+import 'prismjs/components/prism-java';
+import 'prismjs/components/prism-javastacktrace';
+import 'prismjs/components/prism-javascript';
+import 'prismjs/components/prism-json';
+import 'prismjs/components/prism-jsonp';
+import 'prismjs/components/prism-less';
+import 'prismjs/components/prism-markdown';
+import 'prismjs/components/prism-mongodb';
+import 'prismjs/components/prism-powershell';
+import 'prismjs/components/prism-properties';
+import 'prismjs/components/prism-protobuf';
+import 'prismjs/components/prism-python';
+import 'prismjs/components/prism-regex';
+import 'prismjs/components/prism-sass';
+import 'prismjs/components/prism-scss';
+import 'prismjs/components/prism-sql';
+import 'prismjs/components/prism-typescript';
+import 'prismjs/components/prism-yaml';
+import 'prismjs/components/prism-xml-doc';
+import 'prismjs/components/prism-textile';
+
 @Component({
   selector: 'app-add-code-snippet',
   templateUrl: './add-code-snippet.component.html',
@@ -65,6 +103,8 @@ export class AddCodeSnippetComponent implements OnInit {
     { 'language': 'SQL',              'code': 'sql'},
     { 'language': 'Typescript',       'code': 'typescript'},
     { 'language': 'Yaml',             'code': 'yaml'},
+    { 'language': 'XML',              'code': 'xml'},
+    { 'language': 'Plain Text',       'code': 'textile'},
   ]
 
   ngOnInit(): void {
