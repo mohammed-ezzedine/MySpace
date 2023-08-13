4a5dff07666a501c8bce4676586f45af3de3cdbe
Mohammed Ezzedine <mohammed.a.ezzedine@outlook.com>
2023-08-13 23:33:14 +0300

    Added pagination in the frontend

--- MySpace/src/app/components/articles/articles.component.ts
+++ MySpace/src/app/components/articles/articles.component.ts
@@ -13,9 +13,12 @@ export class ArticlesComponent implements OnInit {
 
   articles: Article[] | undefined;
   errorMessage: string = '';
+  pageSize: number = 5;
 
   tag : string | undefined;
   q : string | undefined;
+  currentPage: number = 1;
+  totalNumberOfPages: number = 1;
 
   constructor(private route: ActivatedRoute,
               private articleService: ArticleService,
@@ -40,9 +43,10 @@ export class ArticlesComponent implements OnInit {
   }
 
   private getArticles() {
-    this.articleService.getArticles(this.q, this.tag).subscribe({
+    this.articleService.getArticles(this.currentPage, this.q, this.tag).subscribe({
       next: articles => {
-        this.articles = articles;
+        this.articles = articles.items;
+        this.totalNumberOfPages = articles.totalNumberOfPages;
       },
       error: error => {
         this.errorMessage = error.message;
@@ -50,4 +54,12 @@ export class ArticlesComponent implements OnInit {
     });
   }
 
+  getTotalNumberOfPotentialArticles() {
+    return this.pageSize * this.totalNumberOfPages;
+  }
+
+  jumpToPage(pageNumber: number) {
+    this.currentPage = pageNumber;
+    this.getArticles();
+  }
 }
